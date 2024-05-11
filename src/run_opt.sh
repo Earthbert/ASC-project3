8a6c7054a96cc2e3abdd8b869955af00dc803a89
earthbert <daraban_timotei@tutanota.com>
2024-05-11 16:32:29 +0300

    Add optimized solution
    Signed-off-by: earthbert <daraban_timotei@tutanota.com>

--- src/run_opt.sh
+++ src/run_opt.sh
@@ -2,7 +2,7 @@
 
 make
 
-./tema3_neopt ../input/input
+./tema3_opt_m ../input/input
 
 for i in {1..3}; do
 	./compare out$i ../references/out$i 0.0001
--- src/solver_opt.c
+++ src/solver_opt.c
@@ -3,11 +3,115 @@
  * 2024 Spring
  */
 #include "utils.h"
+#include <stdlib.h>
+#include <string.h>
+
+void multiply_general(double *A, double *B, double *C, int N) {
+	int bi = 0;
+	int bj = 0;
+	int bk = 0;
+	register int i = 0;
+	register int j = 0;
+	register int k = 0;
+	int blockSize = 40;
+
+	for (bi = 0; bi < N; bi += blockSize)
+		for (bj = 0; bj < N; bj += blockSize)
+			for (bk = 0; bk < N; bk += blockSize)
+				for (i = 0; i < blockSize; i++)
+					for (j = 0; j < blockSize; j++) {
+						register double sum = C[(bi + i) * N + bj + j];
+						register double *a = A + (bi + i) * N + bk;
+						register double *b = B + (bk)*N + bj + j;
+						for (k = 0; k < blockSize; k++) {
+							sum += *a * *b;
+							a++;
+							b += N;
+						}
+						C[(bi + i) * N + bj + j] = sum;
+					}
+}
+
+// A is lower triangular
+void multiply_lower_triangular(double *A, double *B, double *C, int N) {
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++) {
+			register double sum = 0;
+			register double *a = A + i * N;
+			register double *b = B + j;
+			for (int k = 0; k <= i; k++) {
+				sum += *a * *b;
+				a++;
+				b += N;
+			}
+			C[i * N + j] = sum;
+		}
+	}
+}
+
+// B is upper triangular
+void multiply_upper_triangular(double *A, double *B, double *C, int N) {
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++) {
+			register double sum = 0;
+			register double *a = A + i * N;
+			register double *b = B + j;
+			for (int k = 0; k <= j; k++) {
+				sum += *a * *b;
+				a++;
+				b += N;
+			}
+			C[i * N + j] = sum;
+		}
+	}
+}
+
+void transpose(double *A, int N) {
+	for (int i = 0; i < N; i++) {
+		for (int j = i + 1; j < N; j++) {
+			double temp = A[i * N + j];
+			A[i * N + j] = A[j * N + i];
+			A[j * N + i] = temp;
+		}
+	}
+}
+
+void add(double *A, double *B, double *C, int N) {
+	for (int i = 0; i < N * N; i++) {
+		C[i] = A[i] + B[i];
+	}
+}
 
 /*
  * Add your optimized implementation here
+	C = (At × B + B x A) x Bt
  */
-double* my_solver(int N, double *A, double* B) {
-	printf("OPT SOLVER\n");
-	return NULL;	
+double *my_solver(int N, double *A, double *B) {
+	double *At = (double *)malloc(N * N * sizeof(double));
+	memcpy(At, A, N * N * sizeof(double));
+	transpose(At, N);
+
+	// C = At x B
+	double *C = (double *)malloc(N * N * sizeof(double));
+	multiply_lower_triangular(At, B, C, N);
+
+	// D = B x A
+	double *D = (double *)malloc(N * N * sizeof(double));
+	multiply_upper_triangular(B, A, D, N);
+
+	// C = (At × B + B x A)
+	add(C, D, C, N);
+
+	// C = (At × B + B x A) x Bt
+	double *Bt = (double *)malloc(N * N * sizeof(double));
+	memcpy(Bt, B, N * N * sizeof(double));
+	transpose(Bt, N);
+	double *result = (double *)malloc(N * N * sizeof(double));
+	multiply_general(C, Bt, result, N);
+
+	free(At);
+	free(C);
+	free(D);
+	free(Bt);
+	return result;
 }
