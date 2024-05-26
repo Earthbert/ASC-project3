7d2f99f3ebbd95e787ef7701e73ba610ecda672f
earthbert <daraban_timotei@tutanota.com>
2024-05-26 14:47:20 +0300

    Improve blas solver
    Signed-off-by: earthbert <daraban_timotei@tutanota.com>

--- src/solver_blas.c
+++ src/solver_blas.c
@@ -3,7 +3,7 @@
  * 2024 Spring
  */
 #include "utils.h"
-#include <cblas.h>
+#include <cblas-atlas.h>
 #include <stdlib.h>
 #include <string.h>
 
@@ -15,19 +15,19 @@ double *my_solver(int N, double *A, double *B) {
 	double *C = (double *)malloc(N * N * sizeof(double));
 	memcpy(C, B, N * N * sizeof(double));
 	// C = At x B
-	cblas_dtrmm(CblasRowMajor, CblasLeft, CblasUpper, CblasTrans, CblasNonUnit, N, N, 1, A, N, C, N);
+	cblas_dtrmm(CblasRowMajor, CblasLeft, CblasUpper, CblasTrans, CblasNonUnit, N, N, 1.0, A, N, C, N);
 
 	double *D = (double *)malloc(N * N * sizeof(double));
 	memcpy(D, B, N * N * sizeof(double));
 	// D = B x A
-	cblas_dtrmm(CblasRowMajor, CblasRight, CblasUpper, CblasNoTrans, CblasNonUnit, N, N, 1, A, N, D, N);
+	cblas_dtrmm(CblasRowMajor, CblasRight, CblasUpper, CblasNoTrans, CblasNonUnit, N, N, 1.0, A, N, D, N);
 
 	// C = (At × B + B x A)
-	cblas_daxpy(N * N, 1, D, 1, C, 1);
+	cblas_daxpy(N * N, 1.0, D, 1.0, C, 1.0);
 
 	// C = (At × B + B x A) x Bt
-	cblas_dgemm(CblasRowMajor, CblasNoTrans, CblasTrans, N, N, N, 1, C, N, B, N, 0, C, N);
+	cblas_dgemm(CblasRowMajor, CblasNoTrans, CblasTrans, N, N, N, 1.0, C, N, B, N, 0.0, D, N);
 
-	free(D);
-	return C;
+	free(C);
+	return D;
 }
